39f50440700516e384c5a69d1b14f41831f58c25
Nicolas Omar Luna <nluna190898@gmail.com>
2026-01-06 16:55:04 -0300

    Configure Next.js for custom base path and export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/my-website',
+  assetPrefix: '/my-website/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
